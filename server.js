573d81388a92d4bc8e7a0ac3031824d520f99cb2
Avinash Kumar Saw <avinashw50w@gmail.com>
2016-12-27 23:58:49 +0530

    changed the position of message-box to the bottom-right

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -10,23 +11,28 @@ connections = [];
 server.listen(process.env.PORT || 8080);
 console.log('server running...');
 
-app.get('/', function(req, res){
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function(socket) {
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
 
-    socket.on('disconnect', function(data){
+    socket.on('disconnect', function(data) {
         users.splice(users.indexOf(socket.username), 1);
         updateUsersList();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets disconnected', connections.length);
     });
 
-    socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg: data, user: socket.username});
+    socket.on('send message', function(data) {
+        io.sockets.emit('new message', {
+            msg: data,
+            user: socket.username
+        });
     });
 
     socket.on('new user', function(data, callback) {
